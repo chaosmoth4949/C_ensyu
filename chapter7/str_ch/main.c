009571d831341b4127e78d8cd40075fc935e5952
chaosmoth4949 <sumakimaou4949@gmail.com>
2021-07-04 15:07:58 +0900

    revise str_ch

--- chapter7/str_ch/main.c
+++ chapter7/str_ch/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #define MAX_SIZE 20
 
 int str_ch(const char *s, char ch);
@@ -7,11 +8,13 @@ void main(){
     char a[MAX_SIZE];
     printf("input string(MAX : %d letters) : ", MAX_SIZE);
     scanf("%s", &a);
-    char *s = &a[0];
+
+    fflush(stdin);
+
     char ch;
     printf("input ch : ");
-    scanf("%s", &ch);
-    printf("最初の %c の添字は %d\n", ch, str_ch(s, ch));
+    scanf("%c", &ch);
+    printf("最初の %c の添字は %d\n", ch, str_ch(a, ch));
 }
 
 int str_ch(const char *s, char ch){
