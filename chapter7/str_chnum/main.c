d19f17a352058182e05c152ccabceb8212c38771
chaosmoth4949 <sumakimaou4949@gmail.com>
2021-07-04 14:59:22 +0900

    revise str_chnum

--- chapter7/str_chnum/main.c
+++ chapter7/str_chnum/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
 #define MAX_SIZE 20
 
 int str_chnum(const char *s, char c);
 
 void main(){
-    char c;
-    printf("input char : ");
-    scanf("%s", &c);
     char a[MAX_SIZE];
     printf("input string(MAX : %d letters) : ", MAX_SIZE);
     scanf("%s", &a);
-    char *s = &a[0];
-    printf("文字列に含まれる %c の個数は %d\n", c, str_chnum(s, c));
+
+    fflush(stdin);
+
+    char c;
+    printf("input char : ");
+    scanf("%c", &c);
+
+    // printf("string : %s, char : %c", a, c);
+
+    printf("文字列に含まれる %c の個数は %d\n", c, str_chnum(a, c));
 }
 
 int str_chnum(const char *s, char c){
+    // printf("string : %s, char : %c", s, c);
+
     int n = 0;
-    for(int i = 0; *(s + i) != '\0'; i++){
-        if(*(s + i) == c) n++;
+    for(const char *a = s; *a; a++){
+        if(*a == c) n++;
     }
     return n;
 }
